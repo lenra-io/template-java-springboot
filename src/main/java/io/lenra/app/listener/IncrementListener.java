cb11f1c2aadb88043865c59d02a0c97babf066c6
jonas.martinez <cbobbxta.fr@gmail.com>
2023-12-15 11:44:24 +0100

    Preparing listener

--- src/main/java/io/lenra/app/listener/IncrementListener.java
+++ src/main/java/io/lenra/app/listener/IncrementListener.java
@@ -1,29 +1,14 @@
 package io.lenra.app.listener;
 
-import io.lenra.app.component.Listener;
-import io.lenra.app.request.ViewRequest;
+import io.lenra.app.request.ListenerRequest;
 
 public class IncrementListener {
-    private int count;
-    private Listener onIncrement;
-
     public IncrementListener() {
     }
 
-    public IncrementListener(int count, Listener onIncrement) {
-        this.count = count;
-        this.onIncrement = onIncrement;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public Listener getOnIncrement() {
-        return onIncrement;
-    }
-
-    public static IncrementListener handle(ViewRequest request) {
-        return new IncrementListener(1, new Listener("onIncrement"));
+    public static IncrementListener handle(ListenerRequest request) {
+        // Execute the listener with the request's props. 
+        // Where is the listener executed from ?
+        return null;
     }
 }
