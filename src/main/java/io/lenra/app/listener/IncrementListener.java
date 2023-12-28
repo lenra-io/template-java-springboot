f2a8b711e90f466ffe0e3242c68646995948ca70
jonas.martinez <cbobbxta.fr@gmail.com>
2023-12-28 11:52:13 +0100

    This is better

--- src/main/java/io/lenra/app/listener/IncrementListener.java
+++ src/main/java/io/lenra/app/listener/IncrementListener.java
@@ -1,5 +1,6 @@
 package io.lenra.app.listener;
 
+import io.lenra.api.internal.ApiException;
 import io.lenra.app.request.ListenerRequest;
 
 public class IncrementListener {
@@ -7,12 +8,21 @@ public class IncrementListener {
     }
 
     public static IncrementListener handle(ListenerRequest request) {
-        // Execute the listener with the request's props. 
+        // Execute the listener with the request's props.
         // Where is the listener executed from ?
 
-        // TODO: How can I use the API object here ? 
+        // TODO: How can I use the API object here ?
 
         System.out.println("IncrementListener.handle() called");
+
+        Object query = new Object();
+
+        try {
+            Object response = request.getApi().getData().coll("test").find(query, query);
+            System.out.println(response.toString());
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
--- src/main/java/io/lenra/app/scripts/GenerateInterfaces.java
+++ src/main/java/io/lenra/app/scripts/GenerateInterfaces.java
@@ -1,29 +1,29 @@
-package io.lenra.app.scripts;
+// package io.lenra.app.scripts;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
+// import com.google.gson.Gson;
+// import com.google.gson.JsonObject;
+// import com.google.gson.JsonSyntaxException;
 
-public class GenerateInterfaces {
-    public static void main(String[] args) throws JsonSyntaxException, IOException {
-        generateInterfaces(args[0], args[1]);
-    }
+// public class GenerateInterfaces {
+//     public static void main(String[] args) throws JsonSyntaxException, IOException {
+//         generateInterfaces(args[0], args[1]);
+//     }
 
-    private static void generateInterfaces(String schemaPath, String outputPath) throws JsonSyntaxException, IOException {
-        Path _schemaPath = Paths.get("api", schemaPath);
-        Path _outputPath = Paths.get("gen", outputPath);
+//     private static void generateInterfaces(String schemaPath, String outputPath) throws JsonSyntaxException, IOException {
+//         Path _schemaPath = Paths.get("api", schemaPath);
+//         Path _outputPath = Paths.get("gen", outputPath);
 
-        Gson gson = new Gson();
-        JsonObject schema = gson.fromJson(Files.readString(_schemaPath), JsonObject.class);
+//         Gson gson = new Gson();
+//         JsonObject schema = gson.fromJson(Files.readString(_schemaPath), JsonObject.class);
 
-        // TODO: Compile the schema into a Java file 
+//         // TODO: Compile the schema into a Java file 
 
         
-        // TODO: Write the Java file to the output path
-    }
-}
+//         // TODO: Write the Java file to the output path
+//     }
+// }
