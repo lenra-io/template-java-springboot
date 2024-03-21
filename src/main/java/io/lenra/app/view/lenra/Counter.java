cc7d026027cc7a210ee9719ce4576c353732c07c
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-03-21 18:09:40 +0100

    refactor: Use constructors

--- src/main/java/io/lenra/app/view/lenra/Counter.java
+++ src/main/java/io/lenra/app/view/lenra/Counter.java
@@ -1,15 +1,13 @@
 package io.lenra.app.view.lenra;
 
-import static io.lenra.app.components.Components.button;
-import static io.lenra.app.components.Components.flex;
-import static io.lenra.app.components.Components.text;
 import java.util.List;
 import java.util.Map;
+
 import io.lenra.app.annotation.AppView;
+import io.lenra.app.components.Button;
 import io.lenra.app.components.Flex;
-import io.lenra.app.components.Flex.CrossAxisAlignment;
-import io.lenra.app.components.Flex.MainAxisAlignment;
 import io.lenra.app.components.Listener;
+import io.lenra.app.components.Text;
 import io.lenra.app.listener.ListenerName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,26 +16,25 @@ import lombok.NonNull;
 import lombok.Setter;
 
 public class Counter {
-  @AppView(prefix = "lenra.")
-  public static Flex counter(List<io.lenra.app.data.Counter> counters, Props props) {
-    var counter = counters.get(0);
-    return flex(
-        List.of(text(props.getText() + ": " + counter.getCount()),
-            button("+",
-                b -> b.setOnPressed(new Listener(ListenerName.INCREMENT.value, Map.of("id", counter.getId()))))),
-        f -> {
-          f.setSpacing(16d);
-          f.setMainAxisAlignment(MainAxisAlignment.SPACE_EVENLY);
-          f.setCrossAxisAlignment(CrossAxisAlignment.CENTER);
-        });
-  }
+	@AppView(prefix = "lenra.")
+	public static Flex counter(List<io.lenra.app.data.Counter> counters, Props props) {
+		var counter = counters.get(0);
+		return new Flex(
+				List.of(
+						new Text(props.getText() + ": " + counter.getCount()),
+						new Button("+")
+								.onPressed(new Listener(ListenerName.INCREMENT.value, Map.of("id", counter.getId())))))
+				.spacing(16d)
+				.mainAxisAlignment(Flex.MainAxisAlignment.SPACE_EVENLY)
+				.crossAxisAlignment(Flex.CrossAxisAlignment.CENTER);
+	}
 
-  @Getter
-  @Setter
-  @AllArgsConstructor
-  @NoArgsConstructor
-  public static class Props {
-    @NonNull
-    private String text;
-  }
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Props {
+		@NonNull
+		private String text;
+	}
 }
--- src/main/java/io/lenra/app/view/lenra/Home.java
+++ src/main/java/io/lenra/app/view/lenra/Home.java
@@ -7,25 +7,26 @@ import java.util.Map;
 import io.lenra.app.annotation.AppView;
 import io.lenra.app.components.Flex.CrossAxisAlignment;
 import io.lenra.app.components.Flex.MainAxisAlignment;
+import io.lenra.app.components.Flex;
 import io.lenra.app.components.LenraComponent;
+import io.lenra.app.components.View;
 import io.lenra.app.components.styles.Direction;
 import io.lenra.app.components.view.definitions.Find;
 import io.lenra.app.view.ViewName;
 
 public class Home {
-  @AppView(prefix = "lenra.")
-  public static LenraComponent home() {
-    return flex(List.of(view(ViewName.LENRA_COUNTER.value, v -> {
-      v.setFind(new Find("counter", Map.of("user", "@me")));
-      v.setProps(new Counter.Props("My personnal counter"));
-    }), view(ViewName.LENRA_COUNTER.value, v -> {
-      v.setFind(new Find("counter", Map.of("user", "global")));
-      v.setProps(new Counter.Props("The common counter"));
-    })), f -> {
-      f.setDirection(Direction.VERTICAL);
-      f.setSpacing(16d);
-      f.setMainAxisAlignment(MainAxisAlignment.SPACE_EVENLY);
-      f.setCrossAxisAlignment(CrossAxisAlignment.CENTER);
-    });
-  }
+	@AppView(prefix = "lenra.")
+	public static LenraComponent home() {
+		return new Flex(List.of(
+				new View(ViewName.LENRA_COUNTER)
+						.find(new Find(io.lenra.app.data.Counter.class, Map.of("user", "@me")))
+						.props(new Counter.Props("My personnal counter")),
+				new View(ViewName.LENRA_COUNTER)
+						.find(new Find(io.lenra.app.data.Counter.class, Map.of("user", "global")))
+						.props(new Counter.Props("The common counter"))))
+				.direction(Direction.VERTICAL)
+				.spacing(16d)
+				.mainAxisAlignment(MainAxisAlignment.SPACE_EVENLY)
+				.crossAxisAlignment(CrossAxisAlignment.CENTER);
+	}
 }
